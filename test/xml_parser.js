d68fdfb1aa609eca2265956b799f1079cd68741e
rc0x03 <chipka01@email.franklin.edu>
2016-05-12 18:34:05 -0400

    test

--- test/xml_parser.js
+++ test/xml_parser.js
@@ -2,11 +2,12 @@ var fs = require('fs');
 
 var libxml = require('../index');
 
-module.exports.parse = function(assert) {
+module.exports.parse = function (assert) {
     var filename = __dirname + '/fixtures/parser.xml';
     var str = fs.readFileSync(filename, 'utf8');
 
     var doc = libxml.parseXml(str);
+
     assert.equal('1.0', doc.version());
     assert.equal('UTF-8', doc.encoding());
     assert.equal('root', doc.root().name());
@@ -21,23 +22,24 @@ module.exports.parse = function(assert) {
     assert.done();
 };
 
-module.exports.parse_buffer = function(assert) {
+module.exports.parse_buffer = function (assert) {
     var filename = __dirname + '/fixtures/parser-utf16.xml';
     var buf = fs.readFileSync(filename);
 
     var doc = libxml.parseXml(buf);
+
     assert.equal('1.0', doc.version());
     assert.equal('UTF-16', doc.encoding());
     assert.equal('root', doc.root().name());
     assert.done();
 };
 
-module.exports.parse_synonym = function(assert) {
+module.exports.parse_synonym = function (assert) {
     assert.strictEqual(libxml.parseXml, libxml.parseXmlString);
     assert.done();
-}
+};
 
-module.exports.recoverable_parse = function(assert) {
+module.exports.recoverable_parse = function (assert) {
     var filename = __dirname + '/fixtures/warnings/ent9.xml';
     var str = fs.readFileSync(filename, 'utf8');
 
@@ -45,6 +47,7 @@ module.exports.recoverable_parse = function(assert) {
 
     assert.equal(1, doc.errors.length);
     var err = doc.errors.shift();
+
     assert.ok(err instanceof Error);
     assert.equal(err.domain, 3);
     assert.equal(err.column, 13);
@@ -55,14 +58,43 @@ module.exports.recoverable_parse = function(assert) {
     assert.done();
 };
 
-module.exports.fatal_error = function(assert) {
+module.exports.baseurl_xml = function (assert) {
+    var str = '<!DOCTYPE example SYSTEM "baseurl.dtd">\n' +
+      '<example msg="&happy;"/>\n';
+
+    // First verify it fails when we don't give baseUrl
+    var doc = libxml.Document.fromXml(str, {
+        dtdvalid: true,
+        nonet: true
+    });
+
+    assert.ok(doc.errors.length > 0);
+
+    // Now it should work
+    var doc = libxml.Document.fromXml(str, {
+        dtdvalid: true,
+        nonet: true,
+        baseUrl: __dirname + '/fixtures/example.xml'
+    });
+
+    assert.ok(!doc.errors || doc.errors.length == 0);
+
+    assert.done();
+};
+
+
+
+
+module.exports.fatal_error = function (assert) {
     var filename = __dirname + '/fixtures/errors/comment.xml';
     var str = fs.readFileSync(filename, 'utf8');
     var err = null;
 
     try {
         libxml.parseXml(str);
-    } catch(e) { err = e; }
+    } catch (e) {
+        err = e;
+    }
 
     var errorControl = {
         domain: 1,
@@ -77,23 +109,25 @@ module.exports.fatal_error = function(assert) {
         int1: null,
         column: 10
     };
+
     assert.equal(errorControl.code, err.code);
     assert.done();
 };
 
-module.exports.parse_options = function(assert) {
+module.exports.parse_options = function (assert) {
     function test_parser_option(input, options, expected) {
         var output = libxml.parseXml(input, options).toString();
+
         output = output.replace(/^<\?xml version="1.0" encoding="UTF-8"\?>\n/, '');
         output = output.replace(/\n$/, '');
         assert.equal(output, expected);
     }
 
-    test_parser_option("<x>&</x>", { recover: true }, "<x/>") // without this option, this document would raise an exception during parsing
-    test_parser_option("<!DOCTYPE x [ <!ENTITY foo 'bar'> ]> <x>&foo;</x>", { noent: true }, '<!DOCTYPE x [\n<!ENTITY foo "bar">\n]>\n<x>bar</x>') // foo => bar
-    test_parser_option("<x> <a>123</a> </x>", { }, "<x> <a>123</a> </x>") // no indentation even though the toString() default called for formatting
-    test_parser_option("<x> <a>123</a> </x>", { noblanks: true }, "<x>\n  <a>123</a>\n</x>") // ah, now we have indentation!
-    test_parser_option("<x><![CDATA[hi]]></x>", {  }, "<x><![CDATA[hi]]></x>") // normally CDATA stays as CDATA
-    test_parser_option("<x><![CDATA[hi]]></x>", { nocdata: true }, "<x>hi</x>") // but here CDATA is removed!
+    test_parser_option("<x>&</x>", { recover: true }, "<x/>"); // without this option, this document would raise an exception during parsing
+    test_parser_option("<!DOCTYPE x [ <!ENTITY foo 'bar'> ]> <x>&foo;</x>", { noent: true }, '<!DOCTYPE x [\n<!ENTITY foo "bar">\n]>\n<x>bar</x>'); // foo => bar
+    test_parser_option("<x> <a>123</a> </x>", { }, "<x> <a>123</a> </x>"); // no indentation even though the toString() default called for formatting
+    test_parser_option("<x> <a>123</a> </x>", { noblanks: true }, "<x>\n  <a>123</a>\n</x>"); // ah, now we have indentation!
+    test_parser_option("<x><![CDATA[hi]]></x>", {  }, "<x><![CDATA[hi]]></x>"); // normally CDATA stays as CDATA
+    test_parser_option("<x><![CDATA[hi]]></x>", { nocdata: true }, "<x>hi</x>"); // but here CDATA is removed!
     assert.done();
 };
